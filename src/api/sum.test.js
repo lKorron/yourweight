d1a4cad707f858e61495f2bc260e69aac64532a3
Korron <lkorron@yandex.ru>
2022-10-19 22:44:07 +0300

    test sum

--- src/api/sum.test.js
+++ src/api/sum.test.js
@@ -1,5 +1,14 @@
 const sum = require("./sum");
 
 test("sum of 2 numbers", () => {
-  expect(sum(2, 3)).toBe(5);
+  expect(0.2 + 0.1).toBeCloseTo(0.3);
+});
+
+const promise = new Promise((resolve, reject) => resolve(3));
+
+describe("lol", () => {
+  test("should End of string g", async () => {
+    const res = await promise;
+    expect(res).toBe(2);
+  });
 });
