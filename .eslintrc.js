14da40dd16111a50e5ca77dbff3ad90ffd62b6f2
Korron <lkorron@yandex.ru>
2022-10-19 12:18:10 +0300

    eslint plugin for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "plugin:prettier/recommended",
+    "plugin:jest/recommended",
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
