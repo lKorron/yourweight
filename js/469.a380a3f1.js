2b52cdf4e9bef9355132b1751ca32727c23df202
Korron <lkorron@yandex.ru>
2022-11-13 14:35:23 +0300

    Initial dist subtree commit

--- js/469.a380a3f1.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkyourweight"]=self["webpackChunkyourweight"]||[]).push([[469],{1469:function(e,t,a){a.r(t),a.d(t,{default:function(){return w}});var u=a(3396),n=a(1923),r=a(3757);a(1598);const c=(0,u.Uk)("Что я сегодня съел?");var s={__name:"EatedTodayView",setup(e){return(e,t)=>((0,u.wg)(),(0,u.j4)(n.Z,{"back-button":"",class:"top-panel"},{header:(0,u.w5)((()=>[c])),default:(0,u.w5)((()=>[(0,u.Wm)(r.Z)])),_:1}))}},o=a(89);const d=(0,o.Z)(s,[["__scopeId","data-v-590ace2b"]]);var w=d}}]);
-//# sourceMappingURL=469.a380a3f1.js.map
