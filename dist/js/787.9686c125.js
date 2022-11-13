2cb6cc35cbbb7c968fe8c74ca182ad402fcc84ae
Korron <lkorron@yandex.ru>
2022-11-13 13:03:21 +0300

    dist init

---
+++ dist/js/787.9686c125.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkyourweight"]=self["webpackChunkyourweight"]||[]).push([[787],{6787:function(e,t,r){r.r(t),r.d(t,{default:function(){return p}});var a=r(3396),s=r(1923),u=r(4843);const n=(0,a.Uk)("Период"),o={class:"period-progress"};var c={__name:"PeriodView",setup(e){return(e,t)=>((0,a.wg)(),(0,a.j4)(s.Z,{"back-button":"",class:"top-panel"},{header:(0,a.w5)((()=>[n])),default:(0,a.w5)((()=>[(0,a._)("div",o,[(0,a.Wm)(u.Z)])])),_:1}))}},d=r(89);const i=(0,d.Z)(c,[["__scopeId","data-v-0bf295ee"]]);var p=i}}]);
+//# sourceMappingURL=787.9686c125.js.map
