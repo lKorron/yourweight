f4dc343bfc3521b575e78ab0f75b6f058b8d6764
Korron <lkorron@yandex.ru>
2022-11-15 12:41:04 +0300

    new build

---
+++ js/469.0b23931e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkyourweight"]=self["webpackChunkyourweight"]||[]).push([[469],{1469:function(e,t,a){a.r(t),a.d(t,{default:function(){return w}});var u=a(6252),n=a(1923),r=a(4648);a(2170);const c=(0,u.Uk)("Что я сегодня съел?");var s={__name:"EatedTodayView",setup(e){return(e,t)=>((0,u.wg)(),(0,u.j4)(n.Z,{"back-button":"",class:"top-panel"},{header:(0,u.w5)((()=>[c])),default:(0,u.w5)((()=>[(0,u.Wm)(r.Z)])),_:1}))}},o=a(3744);const d=(0,o.Z)(s,[["__scopeId","data-v-590ace2b"]]);var w=d}}]);
+//# sourceMappingURL=469.0b23931e.js.map
